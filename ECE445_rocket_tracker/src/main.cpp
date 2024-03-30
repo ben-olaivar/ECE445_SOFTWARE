abfe0c02060583a2cef3bc0d0356c4e126f7233c
Ben <olaivar3@illinois.edu>
2024-03-30 12:28:59 -0500

    menu down button example

--- ECE445_rocket_tracker/src/main.cpp
+++ ECE445_rocket_tracker/src/main.cpp
@@ -3,49 +3,78 @@
 #include "pins.h"
 #include "SparkFun_Ublox_Arduino_Library.h" // http://librarymanager/All#SparkFun_u-blox_GNSS
 
-SFE_UBLOX_GPS myGPS;
-long lastTime = 0;
+
+
+// constants won't change. They're used here to set pin numbers:
+const int down_button_pin = 2;  // the number of the pushbutton pin
+
+// variables will change:
+int down_button_state = 0;  // variable for reading the pushbutton status
+
+int menu_change = 0;  // do we want to change what is being displayed
+int menu_state = 0;   // the current menu being displayed
 
 void setup() {
-  // TODO: remove serial stuff on final version
   Serial.begin(9600);   // open the serial port at 9600 bps:
   while(!Serial);       // wait for serial to start
 
-  Wire.begin();         // join i2c bus (address optional for master)
+  Serial.println("");
+  Serial.println("--------------MENU--------------");
+  Serial.println("> Compass <");
+  Serial.println("Frequency Change");
 
-  //!-------------------GPS STUFF-------------------
-  if (myGPS.begin() == false) //Connect to the Ublox module using Wire port
-  {
-    Serial.println(F("Ublox GPS not detected at default I2C address. Please check wiring. Freezing."));
-    while (1);
-  }
-  //!-------------------END GPS STUFF-------------------
+  // initialize the pushbutton pin as an input:
+  pinMode(down_button_pin, INPUT);
 
-  pinMode(LED_BUILTIN, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
 }
+
+// menu_type corresponds to the menu options being selected
+// 0 = default (select 'compass')
+// 1 = select frequency change
+void print_menu(int menu_type) {
+  Serial.println("");
+  Serial.println("--------------MENU--------------");
+
+  if (menu_type == 0 || menu_type == 1) {
+    Serial.println("> Compass <");
+    Serial.println("Frequency Change");
+  } else if (menu_type == 2) {
+    Serial.println("Compass");
+    Serial.println("> Frequency Change <");
+  }
+}
+
 
 // the loop function runs over and over again forever
 void loop() {
-  // digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  // Serial.print("on\n");
-  // delay(100);                       // wait for a second
-  // digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  // Serial.print("off\n");
-  // delay(100);                       // wait for a second
-  
-  
+  // Serial.println(menu_state);
+  // read the state of the pushbutton value:
+  down_button_state = digitalRead(down_button_pin);
+
+  // check if the pushbutton is pressed. If it is, the down_button_state is HIGH:
+  if (down_button_state == LOW) {
+    // don't change menu state
+    // menu_state = 0;
+    // menu_change = 0;
+
+
+    if (menu_change) {
+      print_menu(0);
+    }
+
+  } else {
+    // frequency change
+    if (menu_state != 2) {
+      menu_change = 1;
+      menu_state = 2;
+    }
+
+    if (menu_change) {
+      print_menu(2);
+      menu_change = 0;
+    }
+    
+  }
 }
 
-struct beacon_transmission_struct {
-  uint8_t error_mask;
-  float coords_longitude;
-  float coords_latitude;
-  float elevation;
-  int callsign;
-};
 
-struct handheld_transmission_struct {
-  int command_number;
-  float command_data;
-  int callsign;
-};
