941af006fcce7f94430e9aac73c216b628303c6b
Ben <olaivar3@illinois.edu>
2024-04-23 20:35:32 -0500

    transmit and receive work at same time

--- ECE445_rocket_tracker/include/beacon_rxtx.cpp
+++ ECE445_rocket_tracker/include/beacon_rxtx.cpp
@@ -3,22 +3,33 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+byte localAddress = 0xBB;
+byte destinationAddress = 0xAA;
+long lastSendTime = 0;
+int interval = 2000;
+int count = 0;
+
+
 struct Data_in {
-    long new_freq;
-    long old_freq;
+    long lat;
+    long lon;
+    char* FAA_id;
 };
 
 struct Data_out {
-    long lat;
-    long lon;
-    char FAA_id;
-};
+    long freq = 433E6;
+    char* FAA_id = "KD9UGU";
+} data_out;
 
-Data_in freqencies = {433, 433};    // defaults 433 just in case
+long Data_out_freqency = 433E6;    // defaults 433 just in case
 
-Data_out beaconData = {0, 0, 0};    // data will default to 0 before proper data can be sent out.
+Data_in beaconData = {0, 0, 0};    // data will default to 0 before proper data can be sent out
+
+// long dummy_freq = 0;        //! this is new requested freq. connect to whatever thr variable is that receives user input
 
 void setup() {
+  Serial.begin(9600);
+  while(!Serial);
   
   LoRa.setPins(10, 9, 2);
   if (!LoRa.begin(433E6)) {
@@ -27,40 +38,104 @@ void setup() {
   }
 }
 
-void loop() {
 
-    //! beaconData.lat = dummy.getLatitude();
-    //! beaconData.lon = dummy.getLongitude();
-    //! dummy is whatever the real GPS object have is named in full code
 
-    LoRa.beginPacket();
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();
+  LoRa.write(destinationAddress);
+  LoRa.write(localAddress);
+  LoRa.write(outgoing.length());
+  LoRa.print(outgoing);
+  LoRa.endPacket();
+}
 
-    LoRa.write((byte *)&beaconData, sizeof(beaconData));
+void receiveMessage(int packetSize) {
+  if (packetSize == 0) return;
 
-    LoRa.endPacket();
+  int recipient = LoRa.read();
+  byte sender = LoRa.read();
+  byte incomingLength = LoRa.read();
 
-    delay(500);
+  String incoming = "";
 
-    // TX ABOVE /\/\/\/\/\/\/\
-    // -----------------------
-    // RX BELOW \/\/\/\/\/\/\/
-  
-    int packetSize = LoRa.parsePacket();
-  
-    if (packetSize) {
-    // received a packet
-    Serial.print("\nReceived packet size ");
-    Serial.print(packetSize);
-    Serial.print(" data ");
-    LoRa.readBytes((byte *)&freqencies.new_freq, packetSize);   // reads received freq into stored data
-
-    if (freqencies.new_freq != freqencies.old_freq) {
-
-        freqencies.old_freq = freqencies.new_freq;         // update what we consider old or current freq
-        LoRa.setFrequency(freqencies.new_freq);            // sets new freq on LoRa
-
-    }
-
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
   }
 
+  if (incomingLength != incoming.length()) {
+    Serial.println("Error: Message length does not match length");
+    return;
+  }
+
+  if (recipient != localAddress) {
+    Serial.println("Error: Recipient address does not match local address");
+    return;
+  }
+
+  Serial.print("Received data " + incoming);
+  Serial.print(" from 0x" + String(sender, HEX));
+  Serial.println(" to 0x" + String(recipient, HEX));
+}
+
+
+// void loop() {
+//   Serial.println("Loop");
+
+//     // if (dummy_freq != Data_out_freqency) { // only send if new freq asked for
+
+//         // Data_out_freqency = dummy_freq;
+
+//         LoRa.beginPacket();
+
+//         LoRa.write((byte *)&data_out, sizeof(data_out));
+
+//         LoRa.endPacket();
+
+//         delay(500);
+
+//     // }
+
+//     // TX ABOVE /\/\/\/\/\/\/\
+//     // -----------------------
+//     // RX BELOW \/\/\/\/\/\/\/
+  
+//     int packetSize = LoRa.parsePacket();
+  
+//     if (packetSize) {
+//     // received a packet
+//     Serial.print("\nReceived packet size ");
+//     Serial.print(packetSize);
+//     Serial.print(" data ");
+//     LoRa.readBytes((byte *)&beaconData, packetSize);   // reads received freq into stored data
+
+//     // print RSSI of packet
+//     Serial.print("' with RSSI ");
+//     Serial.println(LoRa.packetRssi());
+
+//     Serial.print("lat = ");
+//     Serial.print(beaconData.lat);
+
+//     Serial.print("lon = ");
+//     Serial.print(beaconData.lon);
+
+//     Serial.print("FAA_id = ");
+//     Serial.print(beaconData.FAA_id);
+
+//   }
+// }
+
+void loop() {
+  if (millis() - lastSendTime > interval) {
+    String sensorData = String(count++);
+    sendMessage(sensorData);
+
+    Serial.print("Sending data " + sensorData);
+    Serial.print(" from 0x" + String(localAddress, HEX));
+    Serial.println(" to 0x" + String(destinationAddress, HEX));
+
+    lastSendTime = millis();
+    interval = random(2000) + 1000;
+  }
+
+  receiveMessage(LoRa.parsePacket());
 }
--- ECE445_rocket_tracker/include/trackerr_xtx.cpp
+++ ECE445_rocket_tracker/include/trackerr_xtx.cpp
@@ -3,19 +3,33 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+byte localAddress = 0xAA;
+byte destinationAddress = 0xBB;
+long lastSendTime = 0;
+int interval = 2000;
+int count = 0;
+
+
 struct Data_in {
     long lat;
     long lon;
-    char FAA_id;
+    char* FAA_id;
 };
 
-long Data_out_freqency = 433;    // defaults 433 just in case
+struct Data_out {
+    long freq = 433E6;
+    char* FAA_id = "KD9UGU";
+} data_out;
+
+long Data_out_freqency = 433E6;    // defaults 433 just in case
 
 Data_in beaconData = {0, 0, 0};    // data will default to 0 before proper data can be sent out
 
-long dummy_freq = 0;        //! this is new requested freq. connect to whatever thr variable is that receives user input
+// long dummy_freq = 0;        //! this is new requested freq. connect to whatever thr variable is that receives user input
 
 void setup() {
+  Serial.begin(9600);
+  while(!Serial);
   
   LoRa.setPins(10, 9, 2);
   if (!LoRa.begin(433E6)) {
@@ -24,48 +38,101 @@ void setup() {
   }
 }
 
+// void loop() {
+//   Serial.println("Loop");
+
+//     // if (dummy_freq != Data_out_freqency) { // only send if new freq asked for
+
+//         // Data_out_freqency = dummy_freq;
+
+//         LoRa.beginPacket();
+
+//         LoRa.write((byte *)&data_out, sizeof(data_out));
+
+//         LoRa.endPacket();
+
+//         delay(500);
+
+//     // }
+
+//     // TX ABOVE /\/\/\/\/\/\/\
+//     // -----------------------
+//     // RX BELOW \/\/\/\/\/\/\/
+  
+//     int packetSize = LoRa.parsePacket();
+  
+//     if (packetSize) {
+//     // received a packet
+//     Serial.print("\nReceived packet size ");
+//     Serial.print(packetSize);
+//     Serial.print(" data ");
+//     LoRa.readBytes((byte *)&beaconData, packetSize);   // reads received freq into stored data
+
+//     // print RSSI of packet
+//     Serial.print("' with RSSI ");
+//     Serial.println(LoRa.packetRssi());
+
+//     Serial.print("lat = ");
+//     Serial.print(beaconData.lat);
+
+//     Serial.print("lon = ");
+//     Serial.print(beaconData.lon);
+
+//     Serial.print("FAA_id = ");
+//     Serial.print(beaconData.FAA_id);
+
+//   }
+// }
+
 void loop() {
+  if (millis() - lastSendTime > interval) {
+    String sensorData = String(count++);
+    sendMessage(sensorData);
 
-    if (dummy_freq != Data_out_freqency) { // only send if new freq asked for
+    Serial.print("Sending data " + sensorData);
+    Serial.print(" from 0x" + String(localAddress, HEX));
+    Serial.println(" to 0x" + String(destinationAddress, HEX));
 
-        Data_out_freqency = dummy_freq;
-
-        LoRa.beginPacket();
-
-        LoRa.write((byte *)&Data_out_freqency, sizeof(Data_out_freqency));
-
-        LoRa.endPacket();
-
-        delay(500);
-
-    }
-
-    // TX ABOVE /\/\/\/\/\/\/\
-    // -----------------------
-    // RX BELOW \/\/\/\/\/\/\/
-  
-    int packetSize = LoRa.parsePacket();
-  
-    if (packetSize) {
-    // received a packet
-    Serial.print("\nReceived packet size ");
-    Serial.print(packetSize);
-    Serial.print(" data ");
-    LoRa.readBytes((byte *)&beaconData, packetSize);   // reads received freq into stored data
-
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-
-    Serial.print("lat = ");
-    Serial.print(beaconData.lat);
-
-    Serial.print("lon = ");
-    Serial.print(beaconData.lon);
-
-    Serial.print("FAA_id = ");
-    Serial.print(beaconData.FAA_id);
-
+    lastSendTime = millis();
+    interval = random(2000) + 1000;
   }
 
+  receiveMessage(LoRa.parsePacket());
+}
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();
+  LoRa.write(destinationAddress);
+  LoRa.write(localAddress);
+  LoRa.write(outgoing.length());
+  LoRa.print(outgoing);
+  LoRa.endPacket();
+}
+
+void receiveMessage(int packetSize) {
+  if (packetSize == 0) return;
+
+  int recipient = LoRa.read();
+  byte sender = LoRa.read();
+  byte incomingLength = LoRa.read();
+
+  String incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length()) {
+    Serial.println("Error: Message length does not match length");
+    return;
+  }
+
+  if (recipient != localAddress) {
+    Serial.println("Error: Recipient address does not match local address");
+    return;
+  }
+
+  Serial.print("Received data " + incoming);
+  Serial.print(" from 0x" + String(sender, HEX));
+  Serial.println(" to 0x" + String(recipient, HEX));
 }
