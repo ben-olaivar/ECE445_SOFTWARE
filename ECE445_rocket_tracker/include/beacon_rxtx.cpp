3d123d884b809c7ad74edead10c88efd6657a21b
Ben <olaivar3@illinois.edu>
2024-04-24 19:40:37 -0500

    radio working

--- ECE445_rocket_tracker/include/beacon_rxtx.cpp
+++ ECE445_rocket_tracker/include/beacon_rxtx.cpp
@@ -1,37 +1,56 @@
-// Lora transmit a structure
-#include <Wire.h>
+// Lora receive a structure containg seq number and a flaot
+
 #include <SPI.h>
 #include <LoRa.h>
+#include <Wire.h>
+
+// 4011055399719058, -8822548777629069
+struct Data_out {
+    long lat = 401105539;   // dummy vals to start
+    long lon = -882254877;   // ^^^^
+    char* FAA_id = "KD9UGU";
+} beaconData;
 
 struct Data_in {
-    long new_freq;
-    long old_freq;
-};
+    long freq = 433E6;
+    char* FAA_id = "KD9UGU";
+} trackerData;
 
-struct Data_out {
-    long lat;
-    long lon;
-    char FAA_id;
-};
-
-Data_in freqencies = {433, 433};    // defaults 433 just in case
-
-Data_out beaconData = {0, 0, 0};    // data will default to 0 before proper data can be sent out.
+unsigned long timestamp = 0;
 
 void setup() {
-  
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+  Serial.println("LoRa Receiver");
   LoRa.setPins(10, 9, 2);
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1)
+      ;
   }
+  Serial.println("Setup done");
 }
 
 void loop() {
 
-    //! beaconData.lat = dummy.getLatitude();
-    //! beaconData.lon = dummy.getLongitude();
-    //! dummy is whatever the real GPS object have is named in full code
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+
+  if (packetSize) {           // check if got a packet, if yes then read
+
+    LoRa.readBytes((byte *)&trackerData, packetSize);
+
+    Serial.println("Got a new freq:");
+    Serial.println(trackerData.freq);
+
+    LoRa.setFrequency(trackerData.freq);      // change freq based on new incoming freq
+
+    Serial.println("Properly set");
+
+  }
+
+  if (millis() - timestamp > 3000) {        // every 3 seconds sendout current beacon data
 
     LoRa.beginPacket();
 
@@ -39,27 +58,9 @@ void loop() {
 
     LoRa.endPacket();
 
-    delay(500);
+    timestamp = millis();                   // reset 3 second timer
 
-    // TX ABOVE /\/\/\/\/\/\/\
-    // -----------------------
-    // RX BELOW \/\/\/\/\/\/\/
-  
-    int packetSize = LoRa.parsePacket();
-  
-    if (packetSize) {
-    // received a packet
-    Serial.print("\nReceived packet size ");
-    Serial.print(packetSize);
-    Serial.print(" data ");
-    LoRa.readBytes((byte *)&freqencies.new_freq, packetSize);   // reads received freq into stored data
-
-    if (freqencies.new_freq != freqencies.old_freq) {
-
-        freqencies.old_freq = freqencies.new_freq;         // update what we consider old or current freq
-        LoRa.setFrequency(freqencies.new_freq);            // sets new freq on LoRa
-
-    }
+    Serial.println("end beacon data send (dummy vals rn)");
 
   }
 
